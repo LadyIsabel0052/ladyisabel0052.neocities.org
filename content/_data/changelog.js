c75c8f1fc30dcd48a12c9c35fa867d16a24bce21
Isabel <samuel.ofthejorge@gmail.com>
2026-05-12 22:15:29 +0000

    May 12 2026 - Applied to join the Sapphic and Verdantville webrings. Fixed an issue with the 404 page. Hopefully.

--- content/_data/changelog.js
+++ content/_data/changelog.js
@@ -1,4 +1,5 @@
 module.exports = [
   "Jan 24 2026 - Neocities profile created",
-  "May 7 2026 - Page creation"
+  "May 7 2026 - Page creation",
+  "May 12 2026 - Applied to join the Sapphic and Verdantville webrings. Fixed an issue with the 404 page. Hopefully."
 ];
