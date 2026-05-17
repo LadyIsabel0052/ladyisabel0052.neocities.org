e7f2bbee72bf623c01870293d3c3dfa9398fc996
Isabel <samuel.ofthejorge@gmail.com>
2026-05-17 19:23:10 +0000

    May 17 2026 - Joined Verdantville. Joined Null webring.

--- content/_data/changelog.js
+++ content/_data/changelog.js
@@ -2,5 +2,6 @@ module.exports = [
   "Jan 24 2026 - Neocities profile created",
   "May 7 2026 - Page creation",
   "May 12 2026 - Applied to join the Sapphic and Verdantville webrings. Fixed an issue with the 404 page. Hopefully.",
-  "May 13 2026 - Added Atabook guestbook. Joined Sapphic ring. Applied to join Euroring."
+  "May 13 2026 - Added Atabook guestbook. Joined Sapphic ring. Applied to join Euroring.",
+  "May 17 2026 - Joined Verdantville. Joined Null webring."
 ];
